8f58a32ff7ad5fa57ee4b9ddb76e66a96246dd9f
habibaalam <116030336+habibaalam@users.noreply.github.com>
2022-12-20 09:19:42 -0800

    project file created

---
+++ project/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "login.h"
+#include "splash.h"
+int main(void)
+{   ///login();
+    printf("\t\t\tWelcome to The Coffee Shop! \t\t\t\n");
+    printf("");
+    splash();
+    printf("\n");
+    return 0;
+}
